ba848d9f0402b45f61121a303e725b25ba29d1f4
Michael Babaev <yellowblue924@yandex.ru>
2022-09-19 23:46:25 +0300

    отчет по лаб1

--- Lab1/Holsted/src/Main.java
+++ Lab1/Holsted/src/Main.java
@@ -1,26 +1,14 @@
-import java.util.ArrayList;
+import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) throws Exception {
-        testReverse();
-    }
-
-    private static String reverseString(String str) {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String str = scanner.nextLine();
         char[] array = str.toCharArray();
         String result = "";
         for (int i = array.length - 1; i >= 0; i--) {
             result += array[i];
         }
-        return result;
-    }
-
-    private static void testReverse() throws Exception{
-        String[] strings = {"qwerty", "asdf", "zxcv", "", "q"};
-        String[] revertedStrings = {"ytrewq", "fdsa", "vcxz", "", "q"};
-        for (int i = 0; i < strings.length; i++){
-            if(!revertedStrings[i].equals(reverseString(strings[i]))){
-                throw new Exception("Revert test failed!");
-            }
-        }
+        System.out.println(result);
     }
 }
